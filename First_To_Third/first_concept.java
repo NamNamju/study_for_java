ca9e5a8ff84fb1ef9d46ac11d1df530a13203c78
NamNamju <namuju_hi@naver.com>
2019-12-23 21:38:06 +0900

    how to make class

---
+++ First_To_Third/first_concept.java
@@ -0,0 +1,21 @@
+package lecture_one_one;
+
+public class first_concept { // class should exist.
+
+	public static void main(String []args) { // this should exists.
+		
+		System.out.print("Hi, everyone."); // no Enter
+		System.out.println("Nice to meet you."); // Enter
+		System.out.println("Thank you for visiting.");
+		
+		//////////////////////////////////
+		
+		int a = 10;
+		int b = 20;
+		int c = a+b;
+		System.out.println("결과값은 "+c+"입니다.");
+		
+		
+		}
+
+}
