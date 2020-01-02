e309693cdeda33cd8e3eed9f6c957cc298d4427e
NamNamju <namuju_hi@naver.com>
2020-01-02 14:57:51 +0900

    interface and abstract

---
+++ Twelfth/lecture_eleven/Example_one_Abstract.java
@@ -0,0 +1,7 @@
+package lecture_eleven;
+
+public abstract class Example_one_Abstract implements Example_one_Interface{
+
+	public abstract void clear();
+
+}
---
+++ Twelfth/lecture_eleven/Example_one_Interface.java
@@ -0,0 +1,9 @@
+package lecture_eleven;
+
+public interface Example_one_Interface {
+
+	public void add(Object obj);
+	public void update(int index, Object value);
+	public void remove(int index);
+	
+}
