c6ee0ab48d206758d390cb4e4e9b721a91613a14
NamNamju <namuju_hi@naver.com>
2020-01-10 14:10:25 +0900

    value

---
+++ JavaReview/JavaReview/src/com/javaReview/value/Example_1.java
@@ -0,0 +1,32 @@
+package com.javaReview.value;
+
+public class Example_1 {
+
+	public static void main(String[] args) {
+		int number1 = 100;
+		int number2 = 2000;
+		
+		int plus = number2 + number1;
+		int minus = number2 - number1;
+		int multiply = number2 * number1;
+		int divide = number2 / number1;
+		
+		long number3 = 39202901921021L;
+		float number4 = 34.212f;
+		
+		String name = "Namju"; 
+		String addString = " Hi!";
+		
+		System.out.println(number1);
+		System.out.println(plus);
+		System.out.println(minus);
+		System.out.println(multiply);
+		System.out.println(divide);
+		System.out.println(number3);
+		System.out.println(number4);
+		System.out.println(name);
+		System.out.println(name + addString);
+
+	}
+
+}
