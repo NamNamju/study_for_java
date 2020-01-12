f50c8eddbe2d42f489c45a934e9830e3b8a0437f
NamNamju <namuju_hi@naver.com>
2020-01-12 16:09:36 +0900

    main

---
+++ JavaReview/JavaReview/src/com/javaReview/interface_2/Example_1.java
@@ -0,0 +1,10 @@
+package com.javaReview.interface_2;
+
+public class Example_1 {
+
+	public static void main(String []args) {
+		
+		Example_1_browser browser = new Example_1_browser();
+		browser.imgClick();
+	}
+}
