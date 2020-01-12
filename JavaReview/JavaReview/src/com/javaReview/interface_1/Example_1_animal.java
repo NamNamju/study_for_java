f9738cdb1e002982bdcd6109dac334d988fedaf4
NamNamju <namuju_hi@naver.com>
2020-01-12 15:30:41 +0900

    abstract class_animal

---
+++ JavaReview/JavaReview/src/com/javaReview/interface_1/Example_1_animal.java
@@ -0,0 +1,14 @@
+package com.javaReview.interface_1;
+
+public abstract class Example_1_animal {
+
+	// 추상 클래스
+	// 몸통 존재 X
+	
+	public String name;
+	public Example_1_animal(String name) {
+		this.name = name;
+	}
+	public abstract void Cry();
+	
+}
